1de8c236190a3d953b688198e134c9b88f0d6684
Omkar Sawant Bhosle <omkarsbdev@gmail.com>
2025-04-03 09:50:21 -0700

    Init Commit

--- superstructures/ss2_pulse/router.py
+++ superstructures/ss2_pulse/router.py
@@ -1,17 +1,17 @@
 import streamlit as st
 from superstructures.ss3_echo import run_tenant_view
-from superstructures.ss4_root.landlord_view import run_landlord_view
-from superstructures.ss4_root.contractor_view import run_contractor_view
-from protocol_tracker.landten_protocol_tracker_app import render_tracker
+# from superstructures.ss4_root.landlord_view import run_landlord_view
+# from superstructures.ss4_root.contractor_view import run_contractor_view
+from tracker import show_tracker
 
 def route_user(persona: str):
     if persona == "tenant":
         run_tenant_view()
     elif persona == "landlord":
-        run_landlord_view()
+        run_tenant_view()
     elif persona == "contractor":
-        run_contractor_view()
+        run_tenant_view()
     elif persona == "admin":
-        render_tracker()
+        show_tracker()
     else:
         st.error("Invalid persona.")
